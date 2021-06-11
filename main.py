e1cc8fa08c724dad5e94edf59483ca54b1bfb7fc
CompSciMrF <17910442+geekyteacher@users.noreply.github.com>
2021-06-11 12:45:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1 - Add comments to predict what the code will do. Run the code to see if you were correct. 
+
+num1 = input("Enter a number")
+num2 = 10
+
+result = num1 * num2
+
+print(result)
+
+# Task 2 - edit the code to convert the input to an integer. 
